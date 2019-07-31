3aef33eabb6294fae44b58f2cdbb16901f9cf5f0
clean-command-1851 <ed7901979t@gmail.com>
2019-07-31 19:08:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,8 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	monopoly =  {
+	 
+  monopoly =  {
   :railroads=>
   {
     :pieces=>4,
@@ -16,7 +17,17 @@ def monopoly_with_second_tier
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
-    }
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
 
 end
 
